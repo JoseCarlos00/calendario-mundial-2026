2d3a6c74f98433ebd99e7a1d1d3abad1c5cc6ccb
JoseCarlos00 <101861240+JoseCarlos00@users.noreply.github.com>
2026-06-23 14:05:01 -0600

    Feat: Insert Content Dynamic

--- src/pageContent/getData.js
+++ src/pageContent/getData.js
@@ -2,40 +2,78 @@ import { mapCountries } from "./mapCountries.js";
 
 export async function getData() {
 	const response = await fetch(
-		'https://site.api.espn.com/apis/site/v2/sports/soccer/fifa.world/scoreboard?dates=20260611-20260617',
+		'https://site.api.espn.com/apis/site/v2/sports/soccer/fifa.world/scoreboard?dates=20260622-20260629',
 	);
 
 	const data = await response.json();
 
 	return data.events.map((event) => {
-    const localNameOriginal = event.competitions[0].competitors.find((t) => t.homeAway === 'home').team.displayName;
-    const localName = mapCountries.get(localNameOriginal) ?? localNameOriginal;
+    const teamLocal = event.competitions[0].competitors.find((t) => t.homeAway === 'home').team;
+		const localName = mapCountries.get(teamLocal.displayName) ?? teamLocal.displayName;
 
-    const visitanteNameOriginal = event.competitions[0].competitors.find((t) => t.homeAway === 'away').team.displayName;
-    const visitanteName = mapCountries.get(visitanteNameOriginal) ?? visitanteNameOriginal
+    const teamVisitante = event.competitions[0].competitors.find((t) => t.homeAway === 'away').team;
+		const visitanteName = mapCountries.get(teamVisitante.displayName) ?? teamVisitante.displayName;
 
-    const season = event.season.slug;
+		const season = event.season.slug;
 
-    const altGameNoteValue = event.competitions[0].altGameNote;;
-    const altGameNote = season === 'group-stage' ? altGameNoteValue.match(/Group\s(\w)/)?.[1] : altGameNoteValue;
+		const altGameNoteValue = event.competitions[0].altGameNote;
+		const altGameNote = season === 'group-stage' ? altGameNoteValue.match(/Group\s(\w)/)?.[1] : altGameNoteValue;
 
-    console.log('season:', season);
-    
-    return {
+		console.log('season:', season);
+
+		const fechaApi = event.date;
+		const fecha = new Date(fechaApi);
+
+		const longDate = new Intl.DateTimeFormat('es-MX', {
+			weekday: 'long',
+			day: 'numeric',
+			month: 'long',
+			timeZone: 'America/Mexico_City',
+		}).format(fecha);
+
+    const shortDate = new Intl.DateTimeFormat('es-MX', {
+			day: 'numeric',
+			month: 'short',
+			timeZone: 'America/Mexico_City',
+		}).format(fecha);
+
+		const timeDate = new Intl.DateTimeFormat('es-MX', {
+			hour: '2-digit',
+			minute: '2-digit',
+			hour12: false,
+			timeZone: 'America/Mexico_City',
+		}).format(fecha);
+
+    const status = event.status.type.description;
+    const isFulltime = status === 'Full Time';
+    const isScheduled = status === 'Scheduled';
+
+    const marcadorLocal = isScheduled ? '-' : event.competitions[0].competitors.find((t) => t.homeAway === 'home').score;
+    const marcadorVisitante = isScheduled ? '-' : event.competitions[0].competitors.find((t) => t.homeAway === 'away').score;
+
+    const contentSts = isFulltime ? 'FT' : isScheduled ? 'VS' : event.status.displayClock;
+
+		return {
 			id: event.id,
-			fecha: event.date,
+			season,
+			fecha: {
+				longDate,
+				shortDate,
+				timeDate,
+			},
 			local: localName,
-			localLogo: event.competitions[0].competitors.find((t) => t.homeAway === 'home').team.logo,
+			teamLocal,
 			visitante: visitanteName,
-			visitanteLogo: event.competitions[0].competitors.find((t) => t.homeAway === 'away').team.logo,
-			marcadorLocal: event.competitions[0].competitors.find((t) => t.homeAway === 'home').score,
-			marcadorVisitante: event.competitions[0].competitors.find((t) => t.homeAway === 'away').score,
-			estado: event.status.type.description,
+			teamVisitante,
+			marcadorLocal,
+			marcadorVisitante,
+			estado: status,
 			estadio: {
 				name: event.competitions[0].venue.fullName,
 				city: event.competitions[0].venue.address.city,
 			},
 			group: altGameNote,
+			contentSts,
 		};
-  });
+	});
 }
--- src/pageContent/insertContent.js
+++ src/pageContent/insertContent.js
@@ -1,6 +1,53 @@
-import { getData } from "./getData.js";
+import { getData } from './getData.js';
 
 export async function insertContent() {
-  const data = await getData()
-  console.log(data);
+	const data = await getData();
+	console.log(data);
+
+	const tbody = document.querySelector('tbody');
+
+	data.forEach((item, index) => {
+		const tr = document.createElement('tr');
+
+    const img = (src, alt) => `<img
+          src=${src}
+          alt = 'Bandera de ${alt}'
+        />`
+
+		tr.innerHTML = `
+      <td>${index + 1}</td>
+      <td>${item.fecha.longDate}</td>
+      <td>${item.fecha.timeDate} Hrs.</td>
+      <td>${item.group}</td>
+      <td>${item.local}</td>
+      <td>
+        ${
+          !item.teamLocal.isActive
+          ? ''
+          : img(item.teamLocal.logo, item.local)
+        }
+      </td>
+      <td>
+      <div class="versus">
+        <span>${item.marcadorLocal}</span>
+        <span>${item.contentSts}</span>
+        <span>${item.marcadorVisitante}</span>
+      </div>
+      </td>
+      <td>
+        ${
+          !item.teamVisitante.isActive
+          ? ''
+          : img(item.teamVisitante.logo, item.visitante)
+        }
+      
+      </td>
+      <td>${item.visitante}</td>
+
+      <td>${item.estadio.name}</td>
+      <td>${item.estadio.city}</td>
+    `;
+
+		tbody.appendChild(tr);
+	});
 }
